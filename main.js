bf640d2d3a14c6d326f9405167cbd9d8a8f45284
Silas <silas.stg@gmail.com>
2020-07-29 23:09:41 -0300

    create all routes.
    Co-authored-by: lucasharff@hotmail.com <lucasharff@hotmail.com>
    Co-authored-by: pedronardes <p.nardes@gmail.com>
    Co-authored-by: Dirceu Antunes Junior <djjunior1997@hotmail.com>

--- main.js
+++ main.js
@@ -4,25 +4,98 @@ const app = express();
 
 app.use(express.json());
 
-const projects = [];
+const { uuid, isUuid } = require("uuidv4");
 
-const { uuid } = require("uuidv4");
+const scraps = [];
 
-app.get("/projects", (request, response) => {
-//   const { title } = request.query;
- 
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validatescrapId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).
+    json({ error: `This is not a valida UUID` });
+  }
+  next();
+};
+
+function validateScrapInfo(request, response, next) {
+  const { name, message } = request.body;
+
+  if (name !== "" && message !== "") {
+    next();
+  } else {
+    return res.status(400).json({ error: "Scrap sent is empty" });
+  }
+}
+app.get("/projects", logRequests, (request, response) => {
+    const { title } = request.query;
+
+    const results = title
+    ? scraps.filter((scrap) => scrap.title.includes(title))
+    : scraps;
   
-//   const results = title
+    return response.json(results);
+});
 
+app.post("/projects", logRequests, validateScrapInfo, (request, response) => {
+  const { title, owner, age } = request.body;
 
-//     ? projects.filter((project) => project.title.includes(title))
-//     : projects;
+  const scrap = { id: uuid(), title, owner, age };
 
-    return response.send("Hello World");
+  scraps.push(scrap);
+
+  return response.json(scrap);
+});
+
+app.put("/projects/:id", validatescrapId, logRequests,(request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const scrapIndex = scraps.findIndex((scrap) => scrap.id === id);
+
+  if (scrapIndex < 0) {
+    return response.status(400).json({ error: "scrap not find." });
+  }
+
+  const scrap = {
+    id,
+    title,
+    owner,
+  };
+
+  // scraps[scrapIndex] = scrap;
+  scraps[scrapIndex] = scrap;
+
+  return response.json([scrap]);
+  });
+
+app.delete("/projects/:id", validatescrapId, logRequests, (request, response) => {
+  const { id } = request.params;
+
+  const scrapIndex = scraps.findIndex((scrap) => scrap.id === id);
+
+  if (scrapIndex < 0) {
+    return response.status(400).json({ error: "scrap not find." });
+  }
+
+  scraps.splice(scrapIndex, 1);
+  return response.status(204).send();
 });
 
 
-
-app.listen(3333, () => {
+app.listen(5555, () => {
   console.log("Back-end started! 😎");
 });
+
